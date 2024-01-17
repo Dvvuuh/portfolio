4430bfd6b26963eaa0881ed8ac01d0fec6d38767
dvvuuh <dauh13980@gmail.com>
2024-01-17 10:13:17 +0100

    Adding homepage component, fixing sidebar and footer folder tidy up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {SidebarComponent} from "./sidebar/sidebar.component";
-import {FooterComponent} from "./footer/footer.component";
+import {SidebarComponent} from "./common/sidebar/sidebar.component";
+import {FooterComponent} from "./common/footer/footer.component";
+import {HomepageComponent} from "./pages/homepage/homepage.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SidebarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, SidebarComponent, FooterComponent, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
